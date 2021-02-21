6b66bdf3906bf6a0a2d46a3cde2d84ddd52fdc24
ammoore00 <abigailmmoore00@gmail.com>
2021-02-20 19:17:13 -0700

    Fixed unfinished new villages generating

--- src/minecraft/net/minecraft/src/BTAMapGenVillage.java
+++ src/minecraft/net/minecraft/src/BTAMapGenVillage.java
@@ -80,7 +80,7 @@ public class BTAMapGenVillage extends BTAMapGenStructure
     protected StructureStart getStructureStart(int par1, int par2)
     {
     	if (this.worldObj.provider.terrainType == BTAMod.BTAWorldTypeDeco)
-    		return new BTAStructureVillageStart(this.worldObj, this.rand, par1, par2, this.terrainType);
+    		return new StructureVillageStart(this.worldObj, this.rand, par1, par2, this.terrainType);
     	else
     		return new StructureVillageStart(this.worldObj, this.rand, par1, par2, this.terrainType);
     }
