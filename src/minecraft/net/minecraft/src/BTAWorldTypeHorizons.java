0c5fc98c43599033154d7f15f25445bce4b77a13
ammoore00 <abigailmmoore00@gmail.com>
2021-03-02 14:07:30 -0700

    Horizons framework

---
+++ src/minecraft/net/minecraft/src/BTAWorldTypeHorizons.java
@@ -0,0 +1,39 @@
+package net.minecraft.src;
+
+public class BTAWorldTypeHorizons extends WorldType {
+    protected BTAWorldTypeHorizons(int par1, String par2Str) {
+		super(par1, par2Str);
+	}
+
+	/**
+     * Gets the translation key for the name of this world type.
+     */
+    public String getTranslateName()
+    {
+        return "Horizons";
+    }
+    
+    public WorldChunkManager getChunkManager(World world) {
+    	return new BTAHorizonsWorldChunkManager(world);
+    }
+    
+    public IChunkProvider getChunkProviderOverworld(World world, long seed, boolean mapFeaturesEnabled) {
+    	return new BTAHorizonsChunkProvider(world, seed, mapFeaturesEnabled);
+    }
+    
+    public float getCloudHeight() {
+    	return 256F;
+    }
+    
+    public int getAverageGroundLevel() {
+    	return 128;
+    }
+    
+    public double getHorizon() {
+    	return 128F;
+    }
+    
+    public int[] getStrataLevels() {
+    	return new int[] {96, 48, 24};
+    }
+}
