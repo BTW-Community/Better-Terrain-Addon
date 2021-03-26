19a5f7f47995790812d12aa3608c96099ee061d8
ammoore00 <abigailmmoore00@gmail.com>
2021-03-26 14:16:12 -0600

    Removed leftover debug messages

--- src/minecraft_server/net/minecraft/src/BTAWorldType.java
+++ src/minecraft_server/net/minecraft/src/BTAWorldType.java
@@ -17,16 +17,12 @@ public class BTAWorldType extends WorldType {
     public WorldChunkManager getChunkManager(World world, String generatorOptions) {
     	BTAWorldConfigurationInfo info;
     	
-    	System.out.println("Options input: " + generatorOptions);
-    	
     	if (generatorOptions.equals("")) {
     		info = BTAWorldConfigurationInfo.createDefaultConfigurationLegacy(this.isDeco());
     	}
     	else {
     		info = BTAWorldConfigurationInfo.createInfoFromString(generatorOptions);
     	}
-    	
-    	System.out.println("Options output: " + info);
     	
     	return new BTAWorldChunkManager(world, info);
     }
