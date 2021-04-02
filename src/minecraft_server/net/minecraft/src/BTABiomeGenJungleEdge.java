746715785232a31164b334b792d04b7ac1ac1b68
ammoore00 <abigailmmoore00@gmail.com>
2021-04-01 18:12:24 -0600

    1.3.2 server port

--- src/minecraft_server/net/minecraft/src/BTABiomeGenJungleEdge.java
+++ src/minecraft_server/net/minecraft/src/BTABiomeGenJungleEdge.java
@@ -2,16 +2,17 @@ package net.minecraft.src;
 
 import java.util.Random;
 
-public class BTABiomeGenTropics extends BTABiomeGenBase {
-	public BTABiomeGenTropics(int par1, BTAEnumClimate climate)
-	{
-		super(par1, climate);
-		this.btaBiomeDecorator.treesPerChunk = 10;
-		this.btaBiomeDecorator.grassPerChunk = 2;
+public class BTABiomeGenJungleEdge extends BTABiomeGenBase
+{
+    public BTABiomeGenJungleEdge(int par1, BTAEnumClimate climate)
+    {
+        super(par1, climate);
+        this.btaBiomeDecorator.treesPerChunk = 5;
+        this.btaBiomeDecorator.grassPerChunk = 25;
+        this.btaBiomeDecorator.flowersPerChunk = 4;
 		this.btaBiomeDecorator.sandPerChunk = 100;
 		this.btaBiomeDecorator.sandPerChunk2 = 100;
-		this.btaBiomeDecorator.melonChancePerChunk = 32;
-		this.waterColorMultiplier = 65396;
+        this.btaBiomeDecorator.waterlilyPerChunk = 4;
         this.spawnableCreatureList.clear();
         this.spawnableCreatureList.add(new SpawnListEntry(FCEntityChicken.class, 10, 4, 4));
         this.spawnableCreatureList.add(new SpawnListEntry(FCEntityPig.class, 10, 4, 4));
@@ -25,25 +26,16 @@ public class BTABiomeGenTropics extends BTABiomeGenBase {
         this.spawnableMonsterList.add(new SpawnListEntry(FCEntitySlime.class, 10, 4, 4));
         this.spawnableMonsterList.add(new SpawnListEntry(FCEntityEnderman.class, 1, 1, 4));
         this.spawnableMonsterList.add(new SpawnListEntry(FCEntityOcelot.class, 2, 1, 1));
-	}
+        this.waterColorMultiplier = 10083127;
+    }
 
-	/**
-	 * Gets a WorldGen appropriate for this biome.
-	 */
-	@Override
-	public WorldGenerator getRandomWorldGenForTrees(Random rand)
-	{
-    	WorldGenerator gen;
-    	
-    	if (rand.nextInt(2) == 0) {
-    		gen = new BTAWorldGenPalmTreeSmall(false, true);;
-    	}
-    	else {
-    		gen = new WorldGenShrub(3, 3);
-    	}
-    	
-    	return gen;
-	}
+    /**
+     * Gets a WorldGen appropriate for this biome.
+     */
+    public WorldGenerator getRandomWorldGenForTrees(Random par1Random)
+    {
+        return (WorldGenerator)(par1Random.nextInt(10) == 0 ? this.worldGeneratorBigTree : (par1Random.nextInt(2) == 0 ? new WorldGenShrub(3, 0) : (par1Random.nextInt(3) == 0 ? new WorldGenHugeTrees(false, 10 + par1Random.nextInt(20), 3, 3) : new WorldGenTrees(false, 4 + par1Random.nextInt(7), 3, 3, true))));
+    }
 
     /**
      * Gets a WorldGen appropriate for this biome.
@@ -51,5 +43,19 @@ public class BTABiomeGenTropics extends BTABiomeGenBase {
     public WorldGenerator getRandomWorldGenForGrass(Random par1Random)
     {
         return par1Random.nextInt(4) == 0 ? new BTAWorldGenTallGrass(Block.tallGrass.blockID, 2) : new BTAWorldGenTallGrass(Block.tallGrass.blockID, 1);
+    }
+
+    public void decorate(World par1World, Random par2Random, int par3, int par4, BTAWorldConfigurationInfo generatorOptions)
+    {
+        super.decorate(par1World, par2Random, par3, par4, generatorOptions);
+        WorldGenVines var5 = new WorldGenVines();
+
+        for (int var6 = 0; var6 < 50; ++var6)
+        {
+            int var7 = par3 + par2Random.nextInt(16) + 8;
+            byte var8 = 64;
+            int var9 = par4 + par2Random.nextInt(16) + 8;
+            var5.generate(par1World, par2Random, var7, var8, var9);
+        }
     }
 }
