deb69acc33d1123525330000326e08815793564a
Abigail Moore <abigailmmoore00@gmail.com>
2021-05-17 18:46:50 -0600

    Fixed biome ids

--- src/minecraft/net/minecraft/src/BTAEnumClimate.java
+++ src/minecraft/net/minecraft/src/BTAEnumClimate.java
@@ -1,19 +1,21 @@
 package net.minecraft.src;
 
 public enum BTAEnumClimate {
-	SNOWY(-1, 0),
-	COLD(-2, 95),
-	TEMPERATE(-3, 256),
-	TROPICAL(-4, 256),
-	ARID(-5, 256),
-	HELL(-6, 256);
+	SNOWY(-1, 0, true),
+	COLD(-2, 95, true),
+	TEMPERATE(-3, 256, true),
+	TROPICAL(-4, 256, true),
+	ARID(-5, 256, true);
+	//HELL(-6, 256, false);
 	
 	public final int id;
 	public final int minHeightForSnow;
+	public final boolean isOverworld;
 	
-	private BTAEnumClimate(int id, int minHeightForSnow) {
+	private BTAEnumClimate(int id, int minHeightForSnow, boolean isOverworld) {
 		this.id = id;
 		this.minHeightForSnow = minHeightForSnow;
+		this.isOverworld = isOverworld;
 	}
 	
 	public static BTAEnumClimate fromID(int id) {
