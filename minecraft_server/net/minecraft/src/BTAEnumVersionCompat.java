921e45a6d686faf8f3ea11aa04c68fea05704d43
Abigail Moore <abigailmmoore00@gmail.com>
2021-10-07 15:23:03 -0600

    Fixed slate gen

--- minecraft_server/net/minecraft/src/BTAEnumVersionCompat.java
+++ minecraft_server/net/minecraft/src/BTAEnumVersionCompat.java
@@ -15,7 +15,8 @@ public enum BTAEnumVersionCompat {
 	V1_4_3(1, 4, 3),
 	V1_4_4(1, 4, 4),
 	V2_0_0(2, 0, 0),
-	V2_0_1(2, 0, 1);
+	V2_0_1(2, 0, 1),
+	V2_0_2(2, 0, 2);
 	
 	private final int major;
 	private final int minor;
